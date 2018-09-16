13a0e27fd2a9ec0506cfffb45b7368b0d1ae75eb
Dave Moglia <dmoglia@udel.edu>
2018-09-16 15:56:08 -0400

    inital commit test

---
+++ Animal.java
@@ -0,0 +1,18 @@
+
+public class Animal implements Comparable<Animal>{
+	String name;
+	int legs;
+	
+	public int getLegs() {
+		return legs;
+	}
+
+	public String toString() {
+		return name;
+	}
+	
+	@Override
+	public int compareTo(Animal a) {
+		return this.toString().compareTo(a.toString());
+	}
+}
---
+++ MyCompare3.java
@@ -0,0 +1,11 @@
+# CISC275-Fall2018-first-git
+1. Create java files to make this code compile and run.
+
+2. What five objects are created in the main?
+
+the five objects created are dog 1-fido, dog 2-fido, dog3-alfie, the arraylist dogs, and the comparator  
+
+
+3. Can you spot the comparator constructor call? Where is the class definition for the comparator?
+the comparator constructor call is within collections.sort. The class definition for the comparator is within
+the imported Java.util.Comparator
